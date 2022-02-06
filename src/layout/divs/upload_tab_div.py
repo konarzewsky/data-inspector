ab5d9afb8acf82608fd8456162ee376b731beee7
konarzewsky <konarz48@gmail.com>
2022-02-06 21:07:16 +0100

    add loading spinner when uploading data

--- src/layout/divs/upload_tab_div.py
+++ src/layout/divs/upload_tab_div.py
@@ -6,33 +6,39 @@ from src.layout import main_tab_content_style
 def upload_tab_layout():
     return html.Div(
         [
-            dcc.Upload(
-                id="upload-data",
-                children=html.Div(
-                    [
-                        "Drag and drop your file here or ",
-                        html.A("select"),
-                        html.P(
-                            "supported types:    csv    xls    xlsx",
-                            style={"fontSize": 15},
+            dcc.Loading(
+                id="upload-data-loading",
+                type="circle",
+                children=[
+                    dcc.Upload(
+                        id="upload-data",
+                        children=html.Div(
+                            [
+                                "Drag and drop your file here or ",
+                                html.A("select"),
+                                html.P(
+                                    "supported types:    csv    xls    xlsx",
+                                    style={"fontSize": 15},
+                                ),
+                            ]
                         ),
-                    ]
-                ),
-                style={
-                    "width": "90%",
-                    "height": "250px",
-                    "lineHeight": "150px",
-                    "borderWidth": "1px",
-                    "borderStyle": "dashed",
-                    "borderRadius": "5px",
-                    "textAlign": "center",
-                    "verticalAlign": "middle",
-                    "margin": "5%",
-                    "display": "inline-block",
-                    "fontSize": 20,
-                    "backgroundColor": "white",
-                },
-                multiple=True,
+                        style={
+                            "width": "90%",
+                            "height": "250px",
+                            "lineHeight": "150px",
+                            "borderWidth": "1px",
+                            "borderStyle": "dashed",
+                            "borderRadius": "5px",
+                            "textAlign": "center",
+                            "verticalAlign": "middle",
+                            "margin": "5%",
+                            "display": "inline-block",
+                            "fontSize": 20,
+                            "backgroundColor": "white",
+                        },
+                        multiple=True,
+                    ),
+                ],
             ),
             dcc.ConfirmDialog(
                 id="uplod-error",
