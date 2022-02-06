32c2bc776bb4b6cf1bc8329872693222e488e2c4
konarzewsky <konarz48@gmail.com>
2022-02-06 19:15:53 +0100

    organize env config for deployment

--- config/env_config.py
+++ config/env_config.py
@@ -1,3 +1,15 @@
 from dotenv import dotenv_values
+import os
 
+# development: get variables from .env file
 config = dotenv_values(".env")
+
+# production: get variables with os package
+if len(config) == 0:
+    config = {
+        'LINKEDIN_LOGO_URL': os.getenv('LINKEDIN_LOGO_URL'),
+        'LINKEDIN_PROFILE_URL': os.getenv('LINKEDIN_PROFILE_URL'),
+        'GITHUB_LOGO_URL': os.getenv('GITHUB_LOGO_URL'),
+        'GITHUB_PROFILE_URL': os.getenv('GITHUB_PROFILE_URL'),
+        'MAPBOX_ACCESS_TOKEN': os.getenv('MAPBOX_ACCESS_TOKEN'),
+    }
